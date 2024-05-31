23f6086ad09a694f1996731be61a75307d25a25b
I Made Sudarsana Taksa Wibawa <taksawibawa@gmail.com>
2024-05-31 13:56:22 +0800

    bugfix: fix incorrect navigation styling

--- static/massage/js/scripts.js
+++ static/massage/js/scripts.js
@@ -1,12 +1,19 @@
-function toggleSubmenu(element) {
+function toggleSubmenu(event) {
+  let element = event.currentTarget;
+  if (event.target.classList.contains('submenu-item')) {
+    event.stopPropagation();
+    return;
+  }
+
   let submenuList = element.querySelector('.submenu-list');
   let icon = element.querySelector('i');
-  if (submenuList.style.display === 'none') {
-    submenuList.style.display = 'block';
-    icon.classList.add('fa-rotate-90');
-  } else {
+  if (submenuList.style.display === 'block') {
     submenuList.style.display = 'none';
     icon.classList.remove('fa-rotate-90');
+  } else {
+    submenuList.style.display = 'block';
+    console.log('open');
+    icon.classList.add('fa-rotate-90');
   }
 }
 
@@ -16,4 +23,10 @@ document.addEventListener('DOMContentLoaded', (event) => {
     return new bootstrap.Toast(toastEl, { autohide: true });
   });
   toastList.forEach((toast) => toast.show());
+
+  let menuItems = document.querySelectorAll('.menu');
+  menuItems.forEach((menuItem) => {
+    let submenuList = menuItem.querySelector('.submenu-list');
+    submenuList.style.display = 'block';
+  });
 });
